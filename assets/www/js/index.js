aac725c0151d16c629c15a21f4e1fe015dd9021d
Alex Stanese <alecs_theone@yahoo.com>
2014-01-25 18:00:54 +0200

    js modified for new undeprecated transition

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,13 +1,24 @@
-$(document).ready(function() {
-	$( "#fade" ).fadeOut( 1000,"swing");
-});
+$(document).ready(
+	function() {
+		$("#fade").fadeOut( 1500,"swing");
+	}
+);
 	
 //*********************************************************
 // Wait for Cordova to Load
 //*********************************************************
 
+document.addEventListener("deviceready", onDeviceReady, false);
+
 function onDeviceReady() {
 	navigator.splashscreen.hide();
+
+	$("#play").click(function() {
+        // $.mobile.changePage("#page2", { transition: "pop"});
+        $(":mobile-pagecontainer").pagecontainer('change', "#page2", { 
+		transition: 'pop',
+        reverse: true
+		});
+    });  
 }
-document.addEventListener("deviceready", onDeviceReady, false);
 
